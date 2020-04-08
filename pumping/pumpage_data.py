250ce5ea2d14f6f6e2fab1d49aedfe9e836ac4bf
puxia2 <60232107+puxia2@users.noreply.github.com>
2020-04-08 01:03:46 -0500

    part 3 development
    fill gaps and plot

---
+++ pumping/pumpage_data.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  7 17:07:02 2020
+
+@author: Pu
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_excel(r'C:\Users\Pu\Documents\GitHub\GEO572-Project-E.St.Louis\pumping\Pumpage_Data_ESL.xlsx')
+dft = df.T
+dft_pp = dft.iloc[7:,:]
+
+# transform all the pumpage data into float type
+dft_pp = pd.DataFrame(dft_pp,dtype=np.float)
+
+# replace all 0 into nan
+dft_pp = dft_pp.replace(0,np.nan)
+
+# calculate the number of not-null for each year
+print(dft_pp.T.info())
+# print(type(dft_pp.iloc[0,12]))
+
+# linear interpolation
+dft_pp1 = dft_pp.interpolate(method='linear', limit_direction='forward', axis=0)
+dft.iloc[7:,:] = dft_pp1
+# filled dataset
+df_fill = dft.T
+# print(dft_pp1.T.info())
+
+year_list = range(1981,2020)
+year_list_new = [str(x) for x in year_list]
+
+# if all of the pumpage data is nan, then drop the entire row
+df_fill_true = df_fill.dropna(axis=0,how='all',subset=year_list_new)
+
+df_fill_true.to_excel('pumpage data output.xlsx')
+
+
+plt.figure(figsize=(12,8))
+# plot pumpage data of P_number = 402848
+plt.title('p_number = 402848')
+plt.xlabel('year')
+plt.ylabel('pumpage rate (MGD)')
+plt.plot(year_list, df_fill.iloc[44,7:], 'c-*', label='model ready')
+plt.plot(year_list, df.iloc[44,7:], 'ro', label='original')
+plt.legend()
+plt.savefig('p number = 402848')
+plt.show()
+
